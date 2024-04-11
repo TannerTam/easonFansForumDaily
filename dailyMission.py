9fdc002c18f6c813decca5e88247880eeaf91ec8
Tanner <tanner981214@gmail.com>
2024-04-11 20:54:19 +0800

    test

--- dailyMission.py
+++ dailyMission.py
@@ -19,7 +19,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/bin/chromedriver"
+chromedriver = "/usr/local/bin"
 os.environ["webdriver.chrome.driver"] = chromedriver
 service = Service(executable_path=chromedriver, chrome_options=chrome_options)
 driver = webdriver.Chrome(service=service)
