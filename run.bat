a117532f339e2a6bffd9579b0ee09329d138a77d
Tanner <tanner981214@gmail.com>
2024-04-11 19:06:50 +0800

    修改为在Actions中运行

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set LOGFILE=script_log.log
-echo %date:~0,10% %time:~0,8%  script start >> %LOGFILE%
-CALL conda activate easonForum
-python login.py >> %LOGFILE% 2>&1
-echo. >> %LOGFILE%
