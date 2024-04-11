99b6706e0de6a9b4e8c8010c91fe0d821ff1c024
Tanner <tanner981214@gmail.com>
2024-04-11 14:13:18 +0800

    重构了代码，加入了判断操作是否成功

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 set LOGFILE=script_log.log
+echo %date:~0,10% %time:~0,8%  script start >> %LOGFILE%
 CALL conda activate easonForum
 python login.py >> %LOGFILE% 2>&1
-echo %date% %time%: Script execution completed >> %LOGFILE%
 echo. >> %LOGFILE%
