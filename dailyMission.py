52ac4defeb99b6bb18c882d1d602f8260a478508
Tanner <tanner981214@gmail.com>
2024-04-12 10:28:12 +0800

    edified

--- dailyMission.py
+++ dailyMission.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
 from time import sleep
 import re
 import os
@@ -12,19 +13,7 @@ import os
 username = os.environ['USERNAME']
 password = os.environ['PASSWORD']
 
-
-# 模拟浏览器打开网站
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--disable-dev-shm-usage')
-chromedriver = "/usr/local/bin/chromedriver.exe"
-# os.environ["webdriver.chrome.driver"] = chromedriver
-service = Service(executable_path='/usr/local/bin/chromedriver.exe')
-driver = webdriver.Chrome(options=chrome_options)
-
-def login():
+def login(driver):
     # 打开网页
     driver.get("https://www.easonfans.com/forum/plugin.php?id=ahome_dayquestion:index")
 
@@ -47,106 +36,123 @@ def login():
         driver.quit()
         exit() 
 
-def signin():
+def signin(driver):
     # 导航到签到页面
     driver.get("https://www.easonfans.com/forum/plugin.php?id=dsu_paulsign:sign")
-    message_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), '您今天已经签到过了或者签到时间还未开始')]"))
-    )
 
-    if message_element:
-        print("今天已签到。")
-    else:
-        # 等待签到按钮可点击
+    try:
+        # 检查是否已经签到或签到未开始
+        message_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), '您今天已经签到过了或者签到时间还未开始')]"))
+        )
+        print("今天已签到或签到未开始。")
+    except TimeoutException:
+        # 签到按钮可点击，开始签到流程
         WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//a[@onclick=\"showWindow('qwindow', 'qiandao', 'post', '0');return false\"]"))
         )
 
-        # 开始签到
+        # 点击签到触发元素
         li_element = driver.find_element(By.ID, "kx")
         li_element.click()
+
         radio_button = driver.find_element(By.CSS_SELECTOR, "input[type='radio'][name='qdmode'][value='3']")
         radio_button.click()
+
         link = driver.find_element(By.XPATH, "//a[@onclick=\"showWindow('qwindow', 'qiandao', 'post', '0');return false\"]")
         link.click()
 
-        driver.get("https://www.easonfans.com/forum/plugin.php?id=dsu_paulsign:sign")
-        message_element_1 = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), '您今天已经签到过了或者签到时间还未开始')]"))
-        )
-        if message_element_1:
+        # 重新检查是否签到成功
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), '您今天已经签到过了或者签到时间还未开始')]"))
+            )
             print("签到成功！")
-        else:
+        except TimeoutException:
             print("签到失败。")
 
-def question():
-    for i in range(4):
-        driver.get("https://www.easonfans.com/forum/plugin.php?id=ahome_dayquestion:index")
-        participated_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "inner"))
-        )
-        matches = re.search(r"今日已参与 \((\d+)/(\d+)\)", participated_element.text)
-        participated = matches.group(1)
+def question(driver):
+    base_url = "https://www.easonfans.com/forum/plugin.php?id=ahome_dayquestion:index"
+    question_count = 0
+    max_attempts = 3
 
-        if participated == "3":
-            print("今日答题已完成。")
+    while question_count < max_attempts:
+        driver.get(base_url)
+        try:
+            # 等待页面加载完成并检查参与情况
+            participated_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "inner"))
+            )
+            matches = re.search(r"今日已参与 \((\d+)/(\d+)\)", participated_element.text)
+            if matches:
+                participated, total = matches.groups()
+                if participated == total:
+                    print("今日答题已完成。")
+                    break
+                else:
+                    answer_question(driver, int(participated))
+                    question_count += 1
+            else:
+                print("未找到答题信息。")
+                break
+        except Exception as e:
+            print(f"遇到异常：{e}")
             break
 
-        # 等待选项加载完毕    
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "a4"))
-        )
+def answer_question(driver, question_number):
+    # 等待选项加载并点击
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "a4"))).click()
+    # 提交答案
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@name='submit'][@value='true']"))
+    ).click()
+    print(f"回答第 {question_number + 1} 题成功！")
 
-        # 点击单选按钮
-        radio_button = driver.find_element(By.ID, "a4")
-        radio_button.click()
-
-        # 等待提交按钮加载完毕
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@name='submit'][@value='true']"))
-        )
-
-        # 点击提交按钮
-        submit_button = driver.find_element(By.XPATH, "//button[@name='submit'][@value='true']")
-        submit_button.click()
-        print(f"回答第 {participated+1} 题成功！")
-
-def lottery():
-    # 导航到抽奖页面
+def check_free_lottery(driver):
     driver.get("https://www.easonfans.com/forum/plugin.php?id=gplayconstellation:front")
-
-    message_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '今日剩余免费次数：0次')]"))
-    )
-
-    if message_element:
-        print("今天已免费抽奖。")
-    else:
-        # 等待抽奖按钮可点击
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "pointlevel"))
-        )
-
-        # 开始抽奖
-        div_element = driver.find_element(By.ID, "pointlevel")
-        div_element.click()
-        print("开始免费抽奖。")
-        sleep(5)
-
-        # 判断抽奖是否成功
-        driver.get("https://www.easonfans.com/forum/plugin.php?id=gplayconstellation:front")
-
-        message_element_1 = WebDriverWait(driver, 10).until(
+    try:
+        # 等待并检查是否还有剩余的免费抽奖次数
+        message_element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//span[contains(text(), '今日剩余免费次数：0次')]"))
         )
-        if message_element_1:
-            print("免费抽奖成功！")
-        else:
-            print("免费抽奖失败。")
+        return False  # 没有剩余免费抽奖次数
+    except:
+        return True  # 还有剩余免费抽奖次数
+
+def perform_lottery(driver):
+    # 等待抽奖按钮可点击并点击
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "pointlevel"))
+    ).click()
+    print("开始免费抽奖。")
+    sleep(5)  # 等待抽奖结果
+
+def lottery(driver):
+    if not check_free_lottery(driver):
+        print("今天已免费抽奖。")
+        return
+
+    perform_lottery(driver)
+
+    # 重新检查是否抽奖成功
+    if not check_free_lottery(driver):
+        print("免费抽奖成功！")
+    else:
+        print("免费抽奖失败。")
 
 if __name__ == '__main__':
-    login()
-    signin()
-    question()
-    lottery()
+    # 模拟浏览器打开网站
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chromedriver = "/usr/local/bin/chromedriver.exe"
+    # os.environ["webdriver.chrome.driver"] = chromedriver
+    service = Service(executable_path='/usr/local/bin/chromedriver.exe')
+    driver = webdriver.Chrome(options=chrome_options)
+    login(driver)
+    signin(driver)
+    question(driver)
+    lottery(driver)
     driver.quit()  
